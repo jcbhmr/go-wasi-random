544499c323dfedab2b2497f8bedf432a46a15795
Jacob Hummer <jcbhmr@outlook.com>
2025-09-02 06:39:26 +0000

    save

---
+++ random_test.go
@@ -0,0 +1,9 @@
+//go:generate rm -rf ./.out/bindings/ ./insecure/ ./insecure-seed/ ./random/
+//go:generate go tool wit-bindgen-go generate --out ./.out/bindings/ ./wit/
+//go:generate mv ./.out/bindings/wasi/random/insecure/ ./insecure/
+//go:generate mv ./.out/bindings/wasi/random/insecure-seed/ ./insecure-seed/
+//go:generate mv ./.out/bindings/wasi/random/random/ ./random/
+//go:generate rm -rf ./insecure/empty.s ./insecure-seed/empty.s ./random/empty.s
+//go:generate go tool jet -g "*.go" -e "DO NOT EDIT.\n" "DO NOT EDIT.\n\n//go:build wasip2\n" -e "\\.out/bindings/wasi/random/" "" ./insecure/ ./insecure-seed/ ./random/
+
+package random_test
